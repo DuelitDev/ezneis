2788178ce3c0d93cc1588c727419320b0ba17b2e
DuelitDev <jyoon07dev@gmail.com>
2024-11-17 23:09:36 +0900

    Add imports for new modules in models and parsers
    This update includes the addition of new imports for `SchoolInfo`, `SchoolSchedule`, `Meal`, and `Classroom` modules in the `models` package, and their corresponding parsers in the `parsers` package. This change aims to better organize code and improve modularity.

--- ezneis/models/__init__.py
+++ ezneis/models/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+from .school_info import SchoolInfo
+from .schedule import SchoolSchedule
+from .meal import Meal
+from .classroom import Classroom
--- ezneis/parsers/__init__.py
+++ ezneis/parsers/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+from .school_info import SchoolInfoParser
+from .schedule import SchoolScheduleParser
+from .meal import MealParser
+from .classroom import ClassroomParser
