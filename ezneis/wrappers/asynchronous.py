999be2a898d7c497390075662ef83e4eceb4fa84
DuelitDev <jyoon07dev@gmail.com>
2024-11-22 04:00:06 +0900

    wrappers의 사용성 강화
    schedules, meals 서비스 메서드의 date 인자 처리 알고리즘을 개선했습니다. 또한 keyword arguments를 추가하여 사용자가 원하는 인자를 요청에 추가할 수 있도록 했습니다.

--- ezneis/wrappers/asynchronous.py
+++ ezneis/wrappers/asynchronous.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Optional
 from ..http.common import Services
 from ..http.asynchronous import AsyncSession
@@ -17,8 +17,8 @@ class AsyncWrapper:
     def __init__(self, session: AsyncSession):
         self._session = session
 
-    async def get_school_info(self, code: str, region: Optional[Region] = None
-                              ) -> tuple[SchoolInfo, ...]:
+    async def get_school_info(self, code: str, region: Optional[Region] = None,
+                              **kwargs) -> tuple[SchoolInfo, ...]:
         """
         Fetches the school info from school name asynchronously.
 
@@ -33,37 +33,56 @@ class AsyncWrapper:
             "ATPT_OFCDC_SC_CODE": region.value if region is not None else "",
             "SD_SCHUL_CODE" if is_code else "SCHUL_NM": code,
         }
-        data = await self._session.get(**params)
+        data = await self._session.get(**params, **kwargs)
         return tuple(SchoolInfoParser.from_json(i) for i in data)
 
     async def get_schedules(self, code: str, region: Region,
-                            date: Optional[str] = None
-                            ) -> tuple[SchoolSchedule, ...]:
+                            date: Optional[str | tuple[str, str]] = None,
+                            **kwargs) -> tuple[SchoolSchedule, ...]:
         if date is None:
-            date = datetime.today().strftime("%Y%m")
+            start_date = end_date = datetime.today().strftime("%Y%m")
+        elif isinstance(date, str):
+            start_date = end_date = date
+        elif isinstance(date, tuple):
+            start_date, end_date = date
+        else:
+            raise ValueError("date_query 인자가 유효하지 않습니다.")
         data = await self._session.get(
             Services.SCHEDULE,
-            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, AA_YMD=date,
+            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value,
+            AA_FROM_YMD=start_date, AA_TO_YMD=end_date, **kwargs
         )
         return tuple(SchoolScheduleParser.from_json(i) for i in data)
 
     async def get_meals(self, code: str, region: Region,
-                        date: Optional[str] = None) -> tuple[Meal, ...]:
+                        date: Optional[str | tuple[str, str]] = None,
+                        **kwargs) -> tuple[Meal, ...]:
         if date is None:
-            date = datetime.today().strftime("%Y%m%d")
+            today = datetime.today()
+            temp = (today - timedelta(days=today.weekday()))
+            end_date = (temp + timedelta(days=6)).strftime("%Y%m%d")
+            start_date = temp.strftime("%Y%m%d")
+        elif isinstance(date, str):
+            start_date = end_date = date
+        elif isinstance(date, tuple):
+            start_date, end_date = date
+        else:
+            raise ValueError("date_query의 값이 유효하지 않습니다.")
         data = await self._session.get(
             Services.MEAL,
-            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, MLSV_YMD=date,
+            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value,
+            MLSV_FROM_YMD=start_date, MLSV_TO_YMD=end_date, **kwargs
         )
         return tuple(MealParser.from_json(i) for i in data)
 
     async def get_classrooms(self, code: str, region: Region,
-                             year: Optional[int] = None
+                             year: Optional[int] = None, **kwargs
                              ) -> tuple[Classroom, ...]:
         if year is None:
             year = datetime.today().year
         data = await self._session.get(
             Services.CLASSROOMS,
             SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, AY=year,
+            **kwargs
         )
         return tuple(ClassroomParser.from_json(i) for i in data)
--- ezneis/wrappers/synchronous.py
+++ ezneis/wrappers/synchronous.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Optional
 from ..http.common import Services
 from ..http.synchronous import SyncSession
@@ -17,8 +17,8 @@ class SyncWrapper:
     def __init__(self, session: SyncSession):
         self._session = session
 
-    def get_school_info(self, code: str, region: Optional[Region] = None
-                        ) -> tuple[SchoolInfo, ...]:
+    def get_school_info(self, code: str, region: Optional[Region] = None,
+                        **kwargs) -> tuple[SchoolInfo, ...]:
         """
         Fetches the school info from school name.
 
@@ -33,35 +33,56 @@ class SyncWrapper:
             "ATPT_OFCDC_SC_CODE": region.value if region is not None else "",
             "SD_SCHUL_CODE" if is_code else "SCHUL_NM": code,
         }
-        data = self._session.get(**params)
+        data = self._session.get(**params, **kwargs)
         return tuple(SchoolInfoParser.from_json(i) for i in data)
 
     def get_schedules(self, code: str, region: Region,
-                      date: Optional[str] = None) -> tuple[SchoolSchedule, ...]:
+                      date: Optional[str | tuple[str, str]] = None,
+                      **kwargs) -> tuple[SchoolSchedule, ...]:
         if date is None:
-            date = datetime.today().strftime("%Y%m")
+            start_date = end_date = datetime.today().strftime("%Y%m")
+        elif isinstance(date, str):
+            start_date = end_date = date
+        elif isinstance(date, tuple):
+            start_date, end_date = date
+        else:
+            raise ValueError("date_query 인자가 유효하지 않습니다.")
         data = self._session.get(
             Services.SCHEDULE,
-            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, AA_YMD=date,
+            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value,
+            AA_FROM_YMD=start_date, AA_TO_YMD=end_date, **kwargs
         )
         return tuple(SchoolScheduleParser.from_json(i) for i in data)
 
     def get_meals(self, code: str, region: Region,
-                  date: Optional[str] = None) -> tuple[Meal, ...]:
+                  date: Optional[str | tuple[str, str]] = None,
+                  **kwargs) -> tuple[Meal, ...]:
         if date is None:
-            date = datetime.today().strftime("%Y%m%d")
+            today = datetime.today()
+            temp = (today - timedelta(days=today.weekday()))
+            end_date = (temp + timedelta(days=6)).strftime("%Y%m%d")
+            start_date = temp.strftime("%Y%m%d")
+        elif isinstance(date, str):
+            start_date = end_date = date
+        elif isinstance(date, tuple):
+            start_date, end_date = date
+        else:
+            raise ValueError("date_query의 값이 유효하지 않습니다.")
         data = self._session.get(
             Services.MEAL,
-            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, MLSV_YMD=date,
+            SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value,
+            MLSV_FROM_YMD=start_date, MLSV_TO_YMD=end_date, **kwargs
         )
         return tuple(MealParser.from_json(i) for i in data)
 
     def get_classrooms(self, code: str, region: Region,
-                       year: Optional[int] = None) -> tuple[Classroom, ...]:
+                       year: Optional[int] = None, **kwargs
+                       ) -> tuple[Classroom, ...]:
         if year is None:
             year = datetime.today().year
         data = self._session.get(
             Services.CLASSROOMS,
             SD_SCHUL_CODE=code, ATPT_OFCDC_SC_CODE=region.value, AY=year,
+            **kwargs
         )
         return tuple(ClassroomParser.from_json(i) for i in data)
