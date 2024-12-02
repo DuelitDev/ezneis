cfd463d5194d3e82929e1cdff9f32e425d27d1b4
DuelitDev <jyoon07dev@gmail.com>
2024-12-02 12:17:52 +0900

    models/ 및 parsers/ 의 __init__.py 수정
    __init__.py에 자주 사용하는 클래스들을 선언해두어 호출하기 쉽도록 변경했습니다.

--- ezneis/parsers/__init__.py
+++ ezneis/parsers/__init__.py
@@ -3,3 +3,5 @@ from .school_info import SchoolInfoParser
 from .schedule import SchoolScheduleParser
 from .meal import MealParser
 from .classroom import ClassroomParser
+from .time_table import TimetableParser
+from .major import MajorParser
