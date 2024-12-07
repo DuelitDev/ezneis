d639a58ef3e1655c4c44a16ce7d22f746383d674
DuelitDev <jyoon07dev@gmail.com>
2024-12-07 22:05:49 +0900

    docstring 추가

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,9 @@
 try:
     from .key import get_key
-except:
+except ImportError:
+    # noinspection SpellCheckingInspection
+    # noinspection GrazieInspection
+    # PyCharm IDE의 오탈자/문법 관련 기능을 무시
     def get_key():
         raise NotImplementedError("""
         tests/key.py를 구현해야 합니다.
