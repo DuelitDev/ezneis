70586b440820b64ec75304d6448342aea5fe6136
DuelitDev <jyoon07dev@gmail.com>
2024-12-07 18:57:12 +0900

    DepartmentParser 성능 개선

--- ezneis/parsers/department.py
+++ ezneis/parsers/department.py
@@ -13,15 +13,15 @@ __all__ = [
 class DepartmentParser(Parser):
     @classmethod
     def from_json(cls, data: dict) -> Department:
-        name = data["ORD_SC_NM"] if "ORD_SC_NM" in data else None
+        # 주야 과정명
         match data["DGHT_CRSE_SC_NM"]:
             case "주간":       timing = Timing.DAY
             case "야간":       timing = Timing.NIGHT
-            case "산업체특별": timing = Timing.BOTH
-            case _:            timing = None
-        is_special_industry_timing = timing == Timing.BOTH
+            case "산업체특별": timing = Timing.INDUSTRY_SPECIAL
+            case _ as v: raise ValueError(f"처리할 수 없는 주야 과정명: {v}")
+        # 계열명
+        name = data["ORD_SC_NM"]
         return Department(
-            name=name,
             timing=timing,
-            is_special_industry_timing=is_special_industry_timing
+            name=name,
         )
